0110f0317e7b186d67797761af5ff79d39b014c9
DaShinja <byron@dashinja.com>
2019-01-11 18:27:15 -0500

    fix(app.js): Add username and email value capture and push to firebase functionality---

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -5,79 +5,100 @@
 // - Scrolling image box: scrollingImages
 // - Search field:  searchIngredients
 // - Search button: searchBtn
+
 // Submit Form Page:
-// - Title: title-input 
+// - Title: title-input
 // - Ingredients: ingredients-input
 // - Directions: directions-input
 // - Notes: notes-input
 // - ***Image Upload: newImage
+// - User Name: username-input
+// - Email Address: email-input
 // - Submit recipe button:  newRecipeBtn
+
 // Recipe Page:
-// Card: newRecipe 
+// Card: newRecipe
 
 var config = {
-    apiKey: "AIzaSyDDO32RIhsljwPbk3zu5WSBN2WEmEkKNF8",
-    authDomain: "recipeciprocity.firebaseapp.com",
-    databaseURL: "https://recipeciprocity.firebaseio.com",
-    projectId: "recipeciprocity",
-    storageBucket: "recipeciprocity.appspot.com",
-    messagingSenderId: "816681104299"
+  apiKey: 'AIzaSyDDO32RIhsljwPbk3zu5WSBN2WEmEkKNF8',
+  authDomain: 'recipeciprocity.firebaseapp.com',
+  databaseURL: 'https://recipeciprocity.firebaseio.com',
+  projectId: 'recipeciprocity',
+  storageBucket: 'recipeciprocity.appspot.com',
+  messagingSenderId: '816681104299'
 };
 firebase.initializeApp(config);
 
-
 var database = firebase.database();
 
+$('#newRecipeBtn').on('click', function(event) {
+  event.preventDefault();
 
-$("#newRecipeBtn").on("click", function (event) {
-    event.preventDefault();
+  var rTitle = $('#title-input')
+    .val()
+    .trim();
+  var ingred = $('#ingredients-input')
+    .val()
+    .trim();
+  // var img = $("#newImage").val().trim();
+  var dir = $('#directions-input')
+    .val()
+    .trim();
+  var notes = $('#notes-input')
+    .val()
+    .trim();
+  var recDate = moment().format('LLL');
 
-    var rTitle = $("#title-input").val().trim();
-    var ingred = $("#ingredients-input").val().trim();
-    // var img = $("#newImage").val().trim();
-    var dir = $("#directions-input").val().trim();
-    var notes = $("#notes-input").val().trim();
-    var recDate = moment().format('LLL');
+  var user = $('#username-input')
+    .val()
+    .trim();
 
-    // Create local temp object
-    var newRecipe = {
-        Title: rTitle,
-        Ingredients: ingred,
-        Directions: dir,
-        Notes: notes,
-        SubDate: recDate
-    };
+  var email = $('#email-input')
+    .val()
+    .trim();
 
-    // Uploads recipe data to firebase
-    database.ref().push(newRecipe);
+  // Create local temp object
+  var newRecipe = {
+    Title: rTitle,
+    Ingredients: ingred,
+    Directions: dir,
+    Notes: notes,
+    SubDate: recDate,
+    User: user,
+    Email: email
+  };
 
-    // Clear all text-boxes
-    $("#title-input").val("");
-    $("#ingredients-input").val("");
-    // $("#newImage").val("");
-    $("#directions-input").val("");
-    $("#notes-input").val("");
-    
+  // Uploads recipe data to firebase
+  database.ref().push(newRecipe);
+
+  // Clear all text-boxes
+  $('#title-input').val('');
+  $('#ingredients-input').val('');
+  // $("#newImage").val("");
+  $('#directions-input').val('');
+  $('#notes-input').val('');
+  $('#username-input').val('');
+  $('#email-input').val('');
 });
 
 // when a child is added to firebase, retrieve the info
-database.ref().on("child_added", function (childSnap) {
+database.ref().on('child_added', function(childSnap) {
+  // Convenience variables
+  var recTitle = childSnap.val().Title;
+  var recIngred = childSnap.val().Ingredients;
+  var recDirec = childSnap.val().Directions;
+  var recNotes = childSnap.val().Notes;
+  var rec_date = childSnap.val().SubDate;
+  var recUser = childSnap.val().User;
+  var recEmail = childSnap.val.Email;
+  // var newRec
+  // // Create the new Recipe card
+  // // newRec = $("<tr>").append(
+  // //     $("<td>").text(recTitle),
+  // //     $("<td>").text(recIngred),
+  // //     $("<td>").text(recDirec),
+  // //     $("<td>").text(recNotes),
+  // // );
 
-
-    // Convenience variables
-    var recTitle = childSnap.val().Title;
-    var recIngred = childSnap.val().Ingredients;
-    var recDirec = childSnap.val().Directions;
-    var recNotes = childSnap.val().Notes;
-    var rec_date = childSnap.val().SubDate;
-    // var newRec
-    // // Create the new Recipe card
-    // // newRec = $("<tr>").append(
-    // //     $("<td>").text(recTitle),
-    // //     $("<td>").text(recIngred),
-    // //     $("<td>").text(recDirec),
-    // //     $("<td>").text(recNotes),
-    // // );
-
-    // $("#newRecipe").append(newRec);
+  // $("#newRecipe").append(newRec);
 });
